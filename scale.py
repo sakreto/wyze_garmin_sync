a68e1a4c152d4a0a530f2c45fa8211c5201f6c1d
Ricardo <na@gmail.com>
2024-01-19 15:32:57 -0400

    WIP - Fetch and upload historical data

--- scale.py
+++ scale.py
@@ -1,6 +1,7 @@
 #!/usr/local/bin/python3
 import math
 import os
+import datetime
 from wyze_sdk import Client
 from wyze_sdk.errors import WyzeApiError
 from fit import FitEncoder_Weight
@@ -8,36 +9,57 @@ import hashlib
 import garth
 from getpass import getpass
 
-WYZE_EMAIL = os.environ.get('WYZE_EMAIL')
-WYZE_PASSWORD = os.environ.get('WYZE_PASSWORD')
-WYZE_KEY_ID = os.environ.get('WYZE_KEY_ID')
-WYZE_API_KEY = os.environ.get('WYZE_API_KEY')
-GARMIN_USERNAME = os.environ.get('Garmin_username')
-GARMIN_PASSWORD = os.environ.get('Garmin_password')
+def load_env(env_file='creds.env'):
+    os.chdir("wyze_garmin_sync")
+    with open(env_file) as file:
+        for line in file:
+            if line.startswith('#') or not line.strip():
+                continue
+            # Split on the first equals and strip spaces
+            key, value = map(str.strip, line.split('=', 1))
+            os.environ[key] = value
+
+load_env()
+
+WYZE_EMAIL = os.getenv('WYZE_EMAIL')
+WYZE_PASSWORD = os.getenv('WYZE_PASSWORD')
+WYZE_KEY_ID = os.getenv('WYZE_KEY_ID')
+WYZE_API_KEY = os.getenv('WYZE_API_KEY')
+GARMIN_USERNAME = os.getenv('GARMIN_USERNAME')
+GARMIN_PASSWORD = os.getenv('GARMIN_PASSWORD')
+
+# Global variables for start and end dates
+# Example start date: January 1, 2024
+START_DATE = datetime.datetime(2023, 9, 13)
+# Example end date: February 1, 2024
+END_DATE = datetime.datetime(2023, 12, 9)
+
 
 def login_to_wyze():
     try:
-        response = Client().login(email=WYZE_EMAIL, password=WYZE_PASSWORD, key_id=WYZE_KEY_ID, api_key=WYZE_API_KEY)
+        response = Client().login(email=WYZE_EMAIL, password=WYZE_PASSWORD,
+                                  key_id=WYZE_KEY_ID, api_key=WYZE_API_KEY)
         access_token = response.get('access_token')
         return access_token
     except WyzeApiError as e:
         print(f"Wyze API Error: {e}")
         return None
 
+
 def upload_to_garmin(file_path):
     try:
-        garth.resume('/wyze_garmin_sync/tokens')
+        garth.resume('tokens')
         garth.client.username
     except:
         try:
             garth.login(GARMIN_USERNAME, GARMIN_PASSWORD)
-            garth.save('/wyze_garmin_sync/tokens')
+            garth.save('tokens')
         except:
             email = input("Enter Garmin email address: ")
             password = getpass("Enter Garmin password: ")
             try:
                 garth.login(email, password)
-                garth.save('/wyze_garmin_sync/tokens')
+                garth.save('tokens')
             except Exception as exc:
                 print(repr(exc))
                 exit()
@@ -50,109 +72,140 @@ def upload_to_garmin(file_path):
         print(f"Garmin upload error: {e}")
         return False
 
-def generate_fit_file(scale):
+
+def generate_fit_file(record, dry_run=False):
     fit = FitEncoder_Weight()
-    timestamp = math.trunc(scale.latest_records[0].measure_ts / 1000)
-    weight_in_kg = scale.latest_records[0].weight * 0.45359237
+    timestamp = math.trunc(record.measure_ts / 1000)
+    weight_in_kg = record.weight * 0.45359237
 
     data_keys = {
-        'percent_fat': scale.latest_records[0].body_fat,
-        'percent_hydration': scale.latest_records[0].body_water,
-        'visceral_fat_mass': scale.latest_records[0].body_vfr,
-        'bone_mass': scale.latest_records[0].bone_mineral,
-        'muscle_mass': scale.latest_records[0].muscle,
-        'basal_met': scale.latest_records[0].bmr,
-        'physique_rating': scale.latest_records[0].body_type or 5,
-        'active_met': scale.latest_records[0].bmr,
-        'metabolic_age': scale.latest_records[0].metabolic_age,
-        'visceral_fat_rating': scale.latest_records[0].body_vfr,
-        'bmi': scale.latest_records[0].bmi
+        'percent_fat': record.body_fat,
+        'percent_hydration': record.body_water,
+        'visceral_fat_mass': record.body_vfr,
+        'bone_mass': record.bone_mineral,
+        'muscle_mass': record.muscle,
+        'basal_met': record.bmr,
+        'physique_rating': record.body_type or 5,
+        'active_met': record.bmr,
+        'metabolic_age': record.metabolic_age,
+        'visceral_fat_rating': record.body_vfr,
+        'bmi': record.bmi
     }
-    data = {}
-    for key, value in data_keys.items():
-        if value is not None:
-            data[key] = float(value)
-        else:
-            data[key] = None
+
+    data = {key: float(value) if value is not None else None for key,
+            value in data_keys.items()}
     if data.get('basal_met') is None:
         data['active_met'] = None
     else:
-        data['active_met'] = int(float(scale.latest_records[0].bmr) * 1.25)
+        data['active_met'] = int(float(record.bmr) * 1.25)
+
+    # Add the data to the FIT encoder
     fit.write_file_info(time_created=timestamp)
     fit.write_file_creator()
     fit.write_device_info(timestamp=timestamp)
     fit.write_weight_scale(
         timestamp=timestamp,
         weight=weight_in_kg,
-        percent_fat = data.get('percent_fat'),
-        percent_hydration = data.get('percent_hydration'),
-        visceral_fat_mass = data.get('visceral_fat_mass'),
-        bone_mass = data.get('bone_mass'),
-        muscle_mass = data.get('muscle_mass'),
-        basal_met = data.get('basal_met'),
-        physique_rating = data.get('physique_rating'),
-        active_met = data.get('active_met'),
-        metabolic_age = data.get('metabolic_age'),
-        visceral_fat_rating = data.get('visceral_fat_rating'),
-        bmi = data.get('bmi'),
+        percent_fat=data.get('percent_fat'),
+        percent_hydration=data.get('percent_hydration'),
+        visceral_fat_mass=data.get('visceral_fat_mass'),
+        bone_mass=data.get('bone_mass'),
+        muscle_mass=data.get('muscle_mass'),
+        basal_met=data.get('basal_met'),
+        physique_rating=data.get('physique_rating'),
+        active_met=data.get('active_met'),
+        metabolic_age=data.get('metabolic_age'),
+        visceral_fat_rating=data.get('visceral_fat_rating'),
+        bmi=data.get('bmi'),
     )
     fit.finish()
-    with open("wyze_scale.fit", "wb") as fitfile:
-        fitfile.write(fit.getvalue())
 
-def main():
+    # Define the directory and unique file paths based on record's timestamp
+
+    fitfile_path = f"fitfiles/wyze_scale_{timestamp}.fit"
+
+    if dry_run:
+        print("Dry Run: Generated FIT file data for timestamp {}: ".format(timestamp))
+        print(fit.getvalue())  # Print the content of the FIT file
+    else:
+        # Directly write the file without checking for the directory
+        with open(fitfile_path, "wb") as fitfile:
+            fitfile.write(fit.getvalue())
+
+    # Return the path for further processing (e.g., uploading)
+    return fitfile_path
+
+
+def main(dry_run=False):
     access_token = login_to_wyze()
-    os.chdir("/wyze_garmin_sync")
+    print(f"Current working directory: {os.getcwd()}")
+    os.chdir("wyze_garmin_sync")
+    print(f"Current working directory: {os.getcwd()}")
+
     if access_token:
         client = Client(token=access_token)
-        for device in client.devices_list():
+        devices = client.devices_list()
+        # print(f"Found devices: {devices}")
+
+        for device in devices:
             if device.type == "WyzeScale":
                 scale = client.scales.info(device_mac=device.mac)
                 print(f"Scale found with MAC {device.mac}. Latest record is:")
-                print(scale.latest_records)
                 print(f"Body Type: {scale.latest_records[0].body_type or 5}")
 
-                print("Generating fit data...")
-                generate_fit_file(scale)
-                print("Fit data generated...")
+                try:
+                    historical_data = client.scales.get_records(
+                        device_model=device.mac,
+                        start_time=START_DATE,
+                        end_time=END_DATE
+                    )
+                    print(
+                        f"Number of historical records fetched: {len(historical_data)}")
+                except Exception as e:
+                    print(f"Error fetching historical data: {e}")
+                    historical_data = []
 
-                fitfile_path = "/wyze_garmin_sync/wyze_scale.fit"
-                cksum_file_path = "/wyze_garmin_sync/cksum.txt"
+                for record in historical_data:
+                    print(
+                        f"{'Dry Run: ' if dry_run else ''}Processing record for {record}")
 
-                # Calculate checksum of the fit file
-                with open(fitfile_path, "rb") as fitfile:
-                    cksum = hashlib.md5(fitfile.read()).hexdigest()
+                    generate_fit_file(record, dry_run=dry_run)
+                    print("Fit data generated...")
 
-                # Check if cksum.txt exists and read stored checksum
-                if os.path.exists(cksum_file_path):
-                    with open(cksum_file_path, "r") as cksum_file:
-                        stored_cksum = cksum_file.read().strip()
+                    if not dry_run:
+                        fitfile_path = generate_fit_file(
+                            record, dry_run=dry_run)
+                        # Extract the timestamp from the record to name the checksum file
+                        timestamp = math.trunc(record.measure_ts / 1000)
+                        cksum_file_path = f"cksum/cksum_{timestamp}.txt"
 
-                    # Compare calculated checksum with stored checksum
-                    if cksum == stored_cksum:
-                        print("No new measurement")
-                    else:
-                        print("New measurement detected. Uploading file...")
-                        # Upload the fit file to Garmin
-                        if upload_to_garmin(fitfile_path):
-                            print("File uploaded successfully.")
-                            # Update cksum.txt with the new checksum
-                            with open(cksum_file_path, "w") as cksum_file:
-                                cksum_file.write(cksum)
+                        with open(fitfile_path, "rb") as fitfile:
+                            cksum = hashlib.md5(fitfile.read()).hexdigest()
+
+                        upload_needed = False
+
+                        if os.path.exists(cksum_file_path):
+                            with open(cksum_file_path, "r") as cksum_file:
+                                stored_cksum = cksum_file.read().strip()
+
+                            if cksum != stored_cksum:
+                                print("New measurement detected. Uploading file...")
+                                upload_needed = True
+                            else:
+                                print("No new measurement for this record.")
                         else:
-                            print("File upload failed.")
-                else:
-                    print("No chksum detected. Uploading fit file and creating chksum...")
-                    # Upload the fit file to Garmin
-                    if upload_to_garmin(fitfile_path):
-                        print("File uploaded successfully.")
-                        # Create cksum.txt and write the checksum
-                        with open(cksum_file_path, "w") as cksum_file:
-                            cksum_file.write(cksum)
-                        print("cksum.txt created.")
-                    else:
-                        print("File upload failed.")
+                            print(
+                                "No checksum detected for this record. Uploading fit file and creating checksum...")
+                            upload_needed = True
+
+                        if upload_needed:
+                            if upload_to_garmin(fitfile_path):
+                                print("File uploaded successfully.")
+                                with open(cksum_file_path, "w") as cksum_file:
+                                    cksum_file.write(cksum)
+                            else:
+                                print("File upload failed.")
+
 
 if __name__ == "__main__":
-    main()
-
+    main(dry_run=False)  # Set dry_run to False to perform actual processing
